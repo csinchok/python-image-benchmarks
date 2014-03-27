e2a0c15e0765ed6b33382c607de63d94da8e267b
Chris Sinchok <csinchok@theonion.com>
2014-03-27 11:32:14 -0500

    Fixing up the tests

--- pillow_test.py
+++ pillow_test.py
@@ -5,6 +5,6 @@ def crop(filename, selection, width, height, format):
     im = Image.open(filename)
     selection_box = (selection['x0'], selection['y0'], selection['x1'], selection['y1'])
     im = im.crop(selection_box)
-    im = im.resize((width, height))
-    output_filename = "{}.cropped.{}".format(filename, format)
+    im = im.resize((width, height), Image.ANTIALIAS)
+    output_filename = "{}.pillow.{}".format(filename, format)
     im.save(output_filename, format, quality=80)
--- wand_test.py
+++ wand_test.py
@@ -5,11 +5,11 @@ def crop(filename, selection, width, height, format):
     with open(filename, 'rb') as source_file:
         with Image(file=source_file) as img:
             img.crop(selection['x0'], selection['y0'], selection['x1'], selection['y1'])
-            img.transform(resize="{width}x{height}".format(width=width, height=height))
+            img.resize(width, height)
             if format.lower() == 'jpeg':
                 img.format = 'jpeg'
                 img.compression_quality = 80
             if format.lower() == 'png':
                 img.format = 'png'
-            output_filename = "{}.cropped.{}".format(filename, format)
+            output_filename = "{}.wand.{}".format(filename, format)
             img.save(filename=output_filename)
