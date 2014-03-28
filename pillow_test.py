028b0c7a4af7814282fa12cc16b5c1d074fbe43e
Chris Sinchok <csinchok@theonion.com>
2014-03-28 11:26:06 -0500

    Reserve icc profiles

--- pillow_test.py
+++ pillow_test.py
@@ -3,8 +3,11 @@ from PIL import Image
 
 def crop(filename, selection, width, height, format):
     im = Image.open(filename)
+    icc_profile = im.info.get("icc_profile")
     selection_box = (selection['x0'], selection['y0'], selection['x1'], selection['y1'])
     im = im.crop(selection_box)
     im = im.resize((width, height), Image.ANTIALIAS)
     output_filename = "{}.pillow.{}".format(filename, format)
-    im.save(output_filename, format, quality=80)
+    if icc_profile:
+    	im.info["icc_profile"] = icc_profile
+    im.save(output_filename, format, quality=80, icc_profile=icc_profile)
